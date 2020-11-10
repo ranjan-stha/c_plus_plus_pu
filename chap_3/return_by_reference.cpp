7586b1dc59454171b1fa00a687f85bed70783380
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2020-11-10 21:52:59 +0545

    added examples related to function overloading; inline fn; default args fn; return by ref;

---
+++ chap_3/return_by_reference.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * 
+ * Return by Reference
+ * In the following example, the function
+ * returns the reference or the variable at the location
+ * where the function is called.
+ * 
+ * author: ranjan-stha
+ */
+
+ 
+int x;
+
+int& assign(){
+    return x;
+}
+
+int main( ){
+    
+    assign() = 5;
+    
+    cout << x << endl;
+    
+    return 0;
+}
